3788088483fbb87aaaa268c7a0dffaf39ff4629d
NgocBaoTa <tabaongocthachthat8a@gmail.com>
2022-11-04 17:42:40 +0700

    login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,8 +1,78 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import "./login.css";
 import GridViewIcon from "@mui/icons-material/GridView";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { LoginContext } from "../../context/AuthContext";
 
 function Login() {
+  const navigate = useNavigate();
+  const { setLogin, setSignup, signup } = useContext(LoginContext);
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [errMessage, setErrMessage] = useState("");
+
+  // const handleLogin = async (e) => {
+  //   e.preventDefault();
+  //   setErrMessage("");
+
+  //   const data = { email,  password };
+  //   const url = "https://backend-quiz-mindx.herokuapp.com/user/login";
+  //   const response = await fetch(url, {
+  //     method: "POST",
+  //     cache: "no-cache",
+  //     credentials: "same-origin",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     redirect: "follow",
+  //     referrerPolicy: "no-referrer",
+  //     body: JSON.stringify(data),
+  //   });
+
+  //   setLogin(true);
+  //   navigate("/");
+
+  //   console.log("response:", response.clone().json());
+  //   console.log(email, password);
+  //   console.log(response);
+  // };
+const handleLogin = async (e) => {
+    e.preventDefault();
+    setErrMessage("");
+    
+    try {
+      const data = await handleSubmit(email, password);
+      console.log(data);
+      if (data.data && data.data.errCode !== 0) {
+        setErrMessage(data.data.message);
+      } else {
+        localStorage.setItem("user_token", JSON.stringify(data.data.token));
+        setLogin(true);
+        navigate("/");
+      }
+    } catch (e) {
+      if (e.response) {
+        if (e.response.data) {
+          setErrMessage(e.response.data.message);
+        }
+      }
+      console.log("error", e.response);
+    }
+  };
+
+  const handleSubmit = (email, password) => {
+    return axios.post("https://backend-quiz-mindx.herokuapp.com/user/login", {
+      email,
+      password,
+    });
+  };
+ 
+
+  const clickSignup = (e) => {
+    setSignup(!signup);
+  };
+
   return (
     <div className="bg">
       <div className="loginScreen">
@@ -23,16 +93,27 @@ function Login() {
               className="email_input"
               type="email"
               placeholder="Email address"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value.trim());
+              }}
             />
             <label className="password_label">PASSWORD</label>
             <input
               className="password_input"
               type="password"
               placeholder="Password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value.trim());
+              }}
             />
           </div>
 
-          <div className="login_submit">Log In</div>
+          <div className="login_submit" onClick={handleLogin}>
+            Log In
+          </div>
+          {/* <div className="errMessage">{ errMessage}</div> */}
 
           <h4 className="login_forgot">
             Don't have an account?

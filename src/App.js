bcd2ec3c98a7bcb3db0c016c2e07631763b41034
DiecTracLam1 <diectraclam47@gmail.com>
2022-11-04 19:42:18 +0700

    Register Admin

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import './App.css';
-import { Routes, Route } from "react-router-dom";
+// import './App.css';
 import Login from './components/login/Login';
 import Register from './components/register/Register';
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       {/* <Login /> */}
       <Register />
-=======
-      <Routes>
-        <Route path="/user/login" element={<Login />} />
-      </Routes>
->>>>>>> 4adee82de33ff65171cf1cfc17984b14a431e2cc
     </div>
   );
 }
